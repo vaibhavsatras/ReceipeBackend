5f7085b627222531f9d2039317fd78b26b839e58
vaibhavsatras <124855358+vaibhavsatras@users.noreply.github.com>
2024-07-25 21:48:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT =  process.env.PORT || 5000
 app.use(express.json())
 app.use(cors({
 
-    origin:[""],
+    origin:["https://food-receipe-three.vercel.app"],
     methods:["GET","POST"],
     credential: true
 
