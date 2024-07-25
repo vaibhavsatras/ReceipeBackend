27c45e98bf8876ab71d2112e514665df7f88fa7d
vaibhavsatras <124855358+vaibhavsatras@users.noreply.github.com>
2024-07-25 22:07:30 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const Database_Connnect = mongoose.connect(process.env.MONGO_URI)
+const Database_Connnect = mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 try {
     if(!Database_Connnect) throw new Error('Database not Connected...')
