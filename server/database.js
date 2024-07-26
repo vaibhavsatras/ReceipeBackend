b127f62d63df39afdac2f4b724181e97172788a9
vaibhavsatras <124855358+vaibhavsatras@users.noreply.github.com>
2024-07-26 09:27:32 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose')
 
 const Database_Connnect = mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
-mongoose.connection.on('connected',()=>{
+//mongoose.connection.on('connected',()=>{
 
-    console.log('Database Connected Successfully')
+  //  console.log('Database Connected Successfully')
 
-})
+//})
 
-mongoose.connection.on('error',(err)=>{
+//mongoose.connection.on('error',(err)=>{
 
-        console.log(err)
+      //  console.log(err)
 
-})
+//})
 
 module.exports = Database_Connnect
